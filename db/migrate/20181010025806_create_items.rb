3a4c89078f4713e55be7ba4fe7d86627659ec28b
JKuenLiu <jeffkuen.liu@mail.utoronto.ca>
2018-10-09 23:01:22 -0400

    added two models: User and Item User: email, password, first_name, last_name, address, phone Item: status, owner, current_holder, description, start_date, end_date, category

---
+++ db/migrate/20181010025806_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.date :start_date
+      t.date :end_date
+      t.string :category
+      t.string :status
+      t.string :owner
+      t.string :current_holder
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
