5ee703b223d6d7eab46c8b355d0c06ec945bf8f3
Nikola Andrejevic <nikola.andrejevic@mail.utoronto.ca>
2018-10-20 14:21:06 -0400

    Added user authentication functionality using devise and linked to basic homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_025806) do
+ActiveRecord::Schema.define(version: 2018_10_20_173406) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -28,14 +28,13 @@ ActiveRecord::Schema.define(version: 2018_10_10_025806) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "address"
-    t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
