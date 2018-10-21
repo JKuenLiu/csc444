7784a8118781d7d60a24987f0d2ce06061c186a5
JKuenLiu <jeffkuen.liu@mail.utoronto.ca>
2018-10-20 23:00:04 -0400

    fixed db:migration error

--- db/migrate/20181020205840_create_people.rb
+++ db/migrate/20181020205840_create_people.rb
@@ -5,7 +5,6 @@ class CreatePeople < ActiveRecord::Migration[5.2]
       t.string :lname
       t.string :addr
       t.string :phone
-      t.reference :user, foreign_key=true, index=true
 
       t.timestamps
     end
