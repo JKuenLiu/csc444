c217e5b035181edfd6f62cebf01e329f5c045e78
JKuenLiu <jeffkuen.liu@mail.utoronto.ca>
2018-10-26 14:01:43 -0400

    added default images on newly created users

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,7 +5,9 @@ class PeopleController < ApplicationController
 
   def show
     @person = Person.find_by_user_id(current_user.id);
+    add_default_image
   end
+
   def update
     @person = Person.find_by_user_id(current_user.id);
     @person.update(people_params)
@@ -18,5 +20,11 @@ class PeopleController < ApplicationController
   private
     def people_params
       params.require(:person).permit(:fname, :lname, :addr, :phone, :avatar)
+    end
+
+    def add_default_image
+      if !@person.avatar.attached?
+        @person.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.jpg')), filename: 'default_avatar.jpg', content_type: 'image/jpg')
+      end
     end
 end
