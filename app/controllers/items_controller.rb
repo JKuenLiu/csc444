d89c49410da3f0f821fcbd091e44d6c16bb5aade
JKuenLiu <jeffkuen.liu@mail.utoronto.ca>
2018-11-13 15:36:30 -0500

    recommiting the ability for borrow to add borrowing date

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,15 +10,20 @@ class ItemsController < ApplicationController
         end
     end
     def update
-        request_item
+        find_item
+        if validate_dates
+            request_item
+        else
+            #verify_transaction
+            redirect_to @item
+        end
     end
 
     def edit
     end
 
     def show
-        @person = Person.find_by_user_id(current_user.id)
-        @item = Item.find(params[:id])
+        find_item
         verify_transaction
     end
 
@@ -37,8 +42,7 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-        @person = Person.find_by_user_id(current_user.id)
-        @item = @person.items.find(params[:id])
+        find_item
 
         # destroy transactions related to this item
         @transactions = Transaction.where(id: @items.map(&:id))
@@ -49,8 +53,7 @@ class ItemsController < ApplicationController
     end
 
     def request_item
-        @person = Person.find_by_user_id(current_user.id)
-        @item = Item.find(params[:id])
+        find_item
         transaction_params = {:person_id => @person.id, :item_id => params[:id], :date => DateTime.now, :status => :requested}
         @transaction = Transaction.new(transaction_params)
         if @transaction.save
@@ -62,8 +65,7 @@ class ItemsController < ApplicationController
     end
 
     def return_item
-      @person = Person.find_by_user_id(current_user.id)
-      @item = Item.find(params[:id])
+      find_item
       # TODO: Verify here that the items state is borrowed, and current user is the one that has it borrowed
       transaction_params = {:person_id => @person.id, :item_id => params[:id], :date => DateTime.now, :status => :returned}
       @transaction = Transaction.new(transaction_params)
@@ -82,6 +84,31 @@ class ItemsController < ApplicationController
     ##############PRIVATE FUNCTIONS###############
     ##############################################
     private
+
+    def find_item
+        @person = Person.find_by_user_id(current_user.id)
+        @item = Item.find(params[:id])
+    end
+
+    def validate_dates
+        start_date = params[:item][:start_date]
+        end_date = params[:item][:end_date]
+        if !start_date.blank? && !end_date.blank?
+            @item.update(start_date: start_date)
+            @item.update(end_date: end_date)
+        return @item.save
+        end
+        if start_date.blank?
+            @item.errors[:start_date] << "has no date"
+        end
+        if end_date.blank?
+            @item.errors[:end_date] << "has no date"
+        end
+        #puts "--------------failed date"
+        return false
+    end
+
+
     def verify_transaction
         @valid_transaction = true
         if @person.items.include?(@item)
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,18 +8,25 @@ class Item < ApplicationRecord
     #validates :start_date,:presence => true
     #validates :end_date, :presence => true
     validate :start_date_cannot_be_in_the_past,
-             :start_date_cannot_be_later_than_end_date
+             :start_date_cannot_be_later_than_end_date,
+             :start_date_must_have_end_date
 
 
     def start_date_cannot_be_in_the_past
         if start_date and start_date < Date.today
-            errors.add(:start_date, "can't be in the past");
+            errors.add(:start_date, "can't be in the past")
         end
     end
 
     def start_date_cannot_be_later_than_end_date
         if start_date and end_date and start_date > end_date
-            errors.add(:start_date, "can't be past end date");
+            errors.add(:start_date, "can't be after end date")
+        end
+    end
+
+    def start_date_must_have_end_date
+        if start_date and end_date.blank?
+            errors.add(:start_date, "must have an end date")
         end
     end
 end
