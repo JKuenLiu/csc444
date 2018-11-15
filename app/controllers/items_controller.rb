571f9ece9f9fdb592c13c9e51e7ba740c8d56e5b
JKuenLiu <jeffkuen.liu@mail.utoronto.ca>
2018-11-14 19:01:38 -0500

    fixed bug where an approved item could be requested by a third party

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,9 +57,10 @@ class ItemsController < ApplicationController
         transaction_params = {:person_id => @person.id, :item_id => params[:id], :date => DateTime.now, :status => :requested}
         @transaction = Transaction.new(transaction_params)
         if @transaction.save
-            puts 'Success'
+            #puts '-----Success', @transaction.person_id, @transaction.item_id, @transaction.status
+            puts 'Successful Request!'
         else
-            puts 'Error'
+            puts 'Request failed!'
         end
         redirect_to @item
     end
@@ -108,34 +109,91 @@ class ItemsController < ApplicationController
         return false
     end
 
+    def get_cur_item
+        transaction_with_item = Transaction.where(item_id: @item.id).order("date")
+        last_transation_of_item = transaction_with_item.last.date
+        return last_transation_of_item
+    end
+
+    def find_most_recent_return
+        transaction_with_item = Transaction.where(item_id: @item.id).order("date")
+        transaction_with_item_returned = transaction_with_item.where(status: :returned)
+        if transaction_with_item_returned.count > 0
+            last_returned_date = transaction_with_item_returned.last.date
+            return last_returned_date
+        else
+            return nil
+        end
+    end
+
+    def is_a_double_request(requested_transactions)
+        if requested_transactions.where(person_id: @person.id, status: :requested).count > 0
+			return false
+		else
+			return true
+		end
+    end
 
     def verify_transaction
         @valid_transaction = true
+        #if it is the owner, she/he cannot request the item
         if @person.items.include?(@item)
         	@valid_transaction = false
         else
-        	transaction_with_item = Transaction.where(item_id: @item.id).order("date")
-        	if transaction_with_item.count > 0
-        		#check if you have already requested this item
-        		transaction_with_item_returned = transaction_with_item.where(status: :returned)
-        		if transaction_with_item_returned.count > 0
-        			last_returned_date = transaction_with_item_returned.last.date
-        			transactions_after_last_returned_date = transaction_with_item.where("date > ?", last_returned_date)
-        			if transactions_after_last_returned_date.where(person_id: @person.id, status: :requested).count > 0
-        				@valid_transaction = false
-        			else
-        				@valid_transaction = true
-        			end
-        		else
-        			if transaction_with_item.where(person_id: @person.id, status: :requested).count > 0
-        				@valid_transaction = false
-        			else
-        				@valid_transaction = true
-        			end
-        		end
-        	else
-        		@valid_transaction = true
-        	end
+            #in this case you are a borrower requesting an item
+            transaction_with_item = Transaction.where(item_id: @item.id).order("date")
+            if transaction_with_item.count > 0
+                cur_item = transaction_with_item.last
+                #if the item is returned, anyone can borrow the item again
+                if cur_item.returned?
+                    puts "-----------item is returned"
+                    @valid_transaction = true
+                #no one can request an item once it has been borrowed
+                elsif cur_item.approved?
+                    puts "-----------item was approved"
+                    @valid_transaction = false
+                #if the item is currently being requested, then you are only allowed
+                #one request per time the item is out
+                elsif cur_item.requested?
+                    puts "-----------the item is requested"
+                    last_returned_date = find_most_recent_return
+                    if last_returned_date != nil
+                        transactions_after_last_returned_date = transaction_with_item.where("date > ?", last_returned_date)
+                        @valid_transaction = is_a_double_request(transactions_after_last_returned_date)
+                    else
+                        @valid_transaction = is_a_double_request(transaction_with_item)
+                    end
+                #something is fucked if this happens
+                else
+                    puts "something is fucked!"
+                    alert
+                end
+            else
+                puts "-----no valid transactions"
+                @valid_transaction = true
+            end
+        	# transaction_with_item = Transaction.where(item_id: @item.id).order("date")
+        	# if transaction_with_item.count > 0
+        	# 	#check if you have already requested this item
+        	# 	transaction_with_item_returned = transaction_with_item.where(status: :returned)
+        	# 	if transaction_with_item_returned.count > 0
+        	# 		last_returned_date = transaction_with_item_returned.last.date
+        	# 		transactions_after_last_returned_date = transaction_with_item.where("date > ?", last_returned_date)
+        	# 		if transactions_after_last_returned_date.where(person_id: @person.id, status: :requested).count > 0
+        	# 			@valid_transaction = false
+        	# 		else
+        	# 			@valid_transaction = true
+        	# 		end
+        	# 	else
+        	# 		if transaction_with_item.where(person_id: @person.id, status: :requested).count > 0
+        	# 			@valid_transaction = false
+        	# 		else
+        	# 			@valid_transaction = true
+        	# 		end
+        	# 	end
+        	# else
+        	# 	@valid_transaction = true
+        	# end
         end
     end
 
