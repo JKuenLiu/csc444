ebe320bfa11971c3b941ad47a093a5a284a564df
Risa <risa_yoneyama@hotmail.com>
2018-11-20 10:05:20 -0500

    Removed search bar in header.

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 2018_11_19_051440) do
     t.index ["user_id"], name: "index_people_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "transaction_id"
+    t.string "comment"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["transaction_id"], name: "index_reviews_on_transaction_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
