15e8fe28bb0f0013d8eaf9ff7de231d0361669f1
Nikola Andrejevic <nikola.andrejevic@mail.utoronto.ca>
2018-11-21 14:29:04 -0500

    Included rspec and added some tests for homepage controller

---
+++ spec/controllers/homepage_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe HomepageController, type: :controller do
+  describe 'searching' do
+    it 'find all items by item name' do
+      fake_items = [instance_double("Item", :name => "Vacuum Cleaner")]
+      expect(Item).to receive(:where).with("name LIKE ?", "%vacuum%").and_return(fake_items)
+      get :index, params: {:term => "vacuum"}
+      expect(assigns(:all_items)).to eq (fake_items)
+    end
+    it 'find all items by tag name' do
+      fake_sports_tag = instance_double("Tag", :name => "sports")
+      fake_tags = [fake_sports_tag]
+      fake_items = [instance_double("Item", :name => "Basketball")]
+      expect(Tag).to receive(:where).with("name LIKE ?", "%sports%").and_return(fake_tags)
+      expect(fake_sports_tag).to receive(:items).and_return(fake_items)
+      get :index, params: {:term => "sports"}
+      expect(assigns(:all_items)).to eq (fake_items)
+    end
+    it 'find all items no search query' do
+      fake_items = [instance_double("Item", :name => "Basketball")]
+      expect(Item).to receive(:all).and_return(fake_items)
+      get :index
+      expect(assigns(:all_items)).to eq (fake_items)
+    end
+  end
+
+end
