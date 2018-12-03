9a03d5c795db65bb133346272e52c68735e4bdad
Balu Viswanathan <balu.nathan.04@gmail.com>
2018-12-02 19:41:25 -0500

    Finished cleaning migration files.

--- db/migrate/20181202213131_change_people_to_person_in_reports.rb
+++
@@ -1,6 +0,0 @@
-class ChangePeopleToPersonInReports < ActiveRecord::Migration[5.2]
-  def change
-    # remove_reference :reports, :people, foreign_key: true
-    # add_reference :reports, :person, foreign_key: true
-  end
-end
