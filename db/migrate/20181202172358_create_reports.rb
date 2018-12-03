5e280e56ff859748ad8c796f935f9265ba5d05f7
Balu Viswanathan <balu.nathan.04@gmail.com>
2018-12-02 19:22:28 -0500

    WIP: Cleaning migrations.

--- db/migrate/20181202172358_create_reports.rb
+++ db/migrate/20181202172358_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration[5.2]
     create_table :reports do |t|
       t.string :complaint
       t.integer :reporter
-      t.references :offender
+      t.references :person
 
       t.timestamps
     end
--- db/migrate/20181202212650_change_report_table.rb
+++ db/migrate/20181202212650_change_report_table.rb
@@ -1,5 +1,5 @@
 class ChangeReportTable < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :reports, :offender, index: true, foreign_key: true
+    # remove_reference :reports, :offender, index: true, foreign_key: true
   end
 end
