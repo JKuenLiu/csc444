e407f4689cc6aca8f4cee972caac09075fdb4693
Nikola Andrejevic <nikola.andrejevic@mail.utoronto.ca>
2018-12-03 17:15:31 -0500

    Added more categories and added functionality to filter items by categories

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,11 @@ class HomepageController < ApplicationController
     before_action :require_login, only: [:notifications, :history]
 
     def index
+
+        categories = ['All','Accessories', 'Books', 'Clothing', 'Electronics',
+         'Home and Kitchen', 'Luggages and Bags', 'Office Products',
+         'Sports and Outdoors', 'Tools and Home Improvement',
+         'Toys and Games', 'Other']
         @all_items = Array.new
          if params[:term]
              @all_items = Item.where('name LIKE ?', "%#{params[:term]}%")
@@ -11,6 +16,11 @@ class HomepageController < ApplicationController
              end
          else
              @all_items = Item.all
+         end
+
+         if params[:category] && params[:category] != ""
+             puts "Catergory: " + categories[params[:category].to_i] + " " + params[:category]
+             @all_items = @all_items.select{|item|item.category == categories[params[:category].to_i]}
          end
          # @five_star_reviews = find_five_star_reviews
          # @five_star_review_users = get_five_star_review_users
