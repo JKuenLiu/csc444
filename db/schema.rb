91d61928c2f0c9629f7dc157978d767c690b6925
Risa <risa_yoneyama@hotmail.com>
2018-12-03 16:00:06 -0500

    Updated error messages for edit account page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_12_03_051643) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
